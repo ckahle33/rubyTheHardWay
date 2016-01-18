891a03b2296dc297276d28682b80fd5313f7cb9b
Craig Kahle <ckahle33@gmail.com>
2016-01-17 20:23:14 -0600

    Initial commit

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by ruby 
+
+puts "I could have code like this." # comment after ignored 
+
+# You can also use this comment out code 
+# puts "this wont run"
+
+puts "This will run."
